50b18cf5893785600b1b7285beb24ef9d6d5e87d
Fidel Lim <lim.fidel@gmail.com>
2019-12-12 01:36:50 +0400

    generate random number

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,16 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //current value of slider
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!;
+    @IBOutlet weak var randomTarget: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let roundedValue = slider.value.rounded() //round value
+        currentValue = Int(roundedValue)
+        
+        targetValue = Int.random(in: 1...100)
+        randomTarget.text = String(targetValue)
     }
 
     @IBAction func showAlert(){
         
+        let message = "The value of the slider is: \(currentValue)" +
+            "\nThe target value is \(targetValue)"
+        
         //alert popup step1
-        let alert = UIAlertController(title: "Hello World!", message: "This is my first app1", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello World!", message: message, preferredStyle: .alert)
         
         //alert popup step2
         let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
@@ -46,6 +61,13 @@ class ViewController: UIViewController {
         //alert popup step4
         present(alert, animated: true, completion: nil)
         
+    }
+    
+    //get slider value
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now:  \(slider.value) ")
+        let roundedValue = slider.value.rounded() //round value
+        currentValue = Int(roundedValue)
     }
 
 }
