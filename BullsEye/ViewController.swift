1bbe92e7c177293e7d23cc8466476b6f77f5f773
Fidel Lim <lim.fidel@gmail.com>
2019-12-12 16:10:00 +0400

    design main page

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -28,6 +28,23 @@ class ViewController: UIViewController {
         currentValue = Int(roundedValue)
         
         startNewGame()
+        
+        let thumbImageNorml = #imageLiteral(resourceName: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNorml, for: .normal)
+        
+        let thumbImageHighlighted = #imageLiteral(resourceName: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = #imageLiteral(resourceName: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+        
+        let trackRightImage = #imageLiteral(resourceName: "SliderTrackRight")
+        let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+        
     }
 
     @IBAction func showAlert(){
