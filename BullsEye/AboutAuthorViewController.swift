0ac0df5c83907913353091f9e2ee10ea24f1e435
Fidel Lim <lim.fidel@gmail.com>
2019-12-12 13:49:43 +0400

    add new page

---
+++ BullsEye/AboutAuthorViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AboutAuthorViewController.swift
+//  BullsEye
+//
+//  Created by Fidel Lim on 12/12/19.
+//  Copyright © 2019 Fidel Lim. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
