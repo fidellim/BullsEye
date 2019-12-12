435f6cbd7354673d16f77e0032cece93f92fa7d3
Fidel Lim <lim.fidel@gmail.com>
2019-12-12 10:21:10 +0400

    add additional points for accurate answer

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -34,19 +34,23 @@ class ViewController: UIViewController {
         
         //getting difference of current and target value
         let difference = abs(targetValue - currentValue)
-        let points = 100 - difference
+        var points = 100 - difference
         
         score += points
         
         let title: String
         if difference == 0 {
             title = "Perfect!"
+            points += 100
         } else if difference < 5 {
             title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
         } else if difference < 10 {
             title = "Pretty good!"
         } else {
-            title = "Not even close!"
+            title = "Not even close..."
         }
         
         let message = "You scored \(points) points"
